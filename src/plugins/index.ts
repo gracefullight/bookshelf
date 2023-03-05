e4d850b8c1474cdac34bf8297950b09a90987c34
gracefullight <gracefullight.dev@gmail.com>
2023-03-05 21:18:46 +0900

    feat: ✨ jpub

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,4 +3,5 @@ export { default as bjpublic } from "./bjpublic";
 export { default as easyspub } from "./easyspub";
 export { default as hanbit } from "./hanbit";
 export { default as insight } from "./insight";
+export { default as jpub } from "./jpub";
 export { default as wikibook } from "./wikibook";
---
+++ src/plugins/jpub.ts
@@ -0,0 +1,9 @@
+import type { PluginOptions } from "../types";
+
+const options: PluginOptions = {
+  name: "jpub",
+  type: "file",
+  url: "https://jpub.tistory.com/feed",
+};
+
+export default options;
